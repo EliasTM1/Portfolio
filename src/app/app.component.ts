a54eaa824b7c52b9bb4c1b62181e5149b52550cd
Elias Toscano <mrtoscano94@gmail.com>
2024-02-18 00:01:04 -0500

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as Aos from 'aos';
-
+import {init} from "aos";
 
 @Component({
   selector: 'app-root',
@@ -11,7 +10,7 @@ export class AppComponent implements OnInit {
   title = 'portfolio';
 
   ngOnInit(): void {
-    Aos.init();
+    init({startEvent: 'DOMContentLoaded'});
   }
 
 }
